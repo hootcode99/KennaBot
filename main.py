a2fa1dc1c18c3e9b9231f5ce78fc08121d7876e3
Williams, Michael Owen <89938996+hootcode99@users.noreply.github.com>
2022-07-06 21:29:24 -0500

    initial commit

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-# This is a sample Python script.
+import tweepy
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Key, Tokens, and Secrets
+API_KEY = "ZcyTQItoXFoiqZv3ax0oQ10fT"
+API_SECRET = "Zq3j7Bw4eU3U2jqsp0EClqDeQhKo6QtXFx7VSTddL7oYV0wXQ8"
+ACCESS_TOKEN = "2155959079-Khc9DWv4z4U1JcjdqzJy2qAyCxb3ERdJYEaO7Ph"
+ACCESS_TOKEN_SECRET = "WlC0F8YOlsIP55JI1HEW6YcTylMueO1b2oiOTMHqKq1RF"
+BEARER_TOKEN = "AAAAAAAAAAAAAAAAAAAAAPYregEAAAAAotLrlCG%2B3TIY1TcOau8" \
+               "iNqObxm0%3D3Fj4g334oYZvkaL6YMPZXJofOCGsZP9qyWDGgcKqBn94jbQiuX"
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Authenticate to Twitter
+# auth = tweepy.OAuthHandler(API_KEY, API_SECRET)
+# auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+#
+# api = tweepy.API(auth)
+client = tweepy.Client(BEARER_TOKEN, API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+new_tweet = client.create_tweet(text='First tweet from #python !')
