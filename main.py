cc7cf1edf166a4e6d90f84f1ffe0efa89420ffca
Williams, Michael Owen <89938996+hootcode99@users.noreply.github.com>
2022-07-07 16:03:30 -0500

    Working Version
    Reply functions
    Need to test timer

--- main.py
+++ main.py
@@ -1,17 +1,47 @@
 import tweepy
+import time
 
-# Key, Tokens, and Secrets
-API_KEY = "ZcyTQItoXFoiqZv3ax0oQ10fT"
-API_SECRET = "Zq3j7Bw4eU3U2jqsp0EClqDeQhKo6QtXFx7VSTddL7oYV0wXQ8"
-ACCESS_TOKEN = "2155959079-Khc9DWv4z4U1JcjdqzJy2qAyCxb3ERdJYEaO7Ph"
-ACCESS_TOKEN_SECRET = "WlC0F8YOlsIP55JI1HEW6YcTylMueO1b2oiOTMHqKq1RF"
-BEARER_TOKEN = "AAAAAAAAAAAAAAAAAAAAAPYregEAAAAAotLrlCG%2B3TIY1TcOau8" \
-               "iNqObxm0%3D3Fj4g334oYZvkaL6YMPZXJofOCGsZP9qyWDGgcKqBn94jbQiuX"
+# Store Key, Tokens, and Secrets
+API_KEY = ""
+API_SECRET = ""
+ACCESS_TOKEN = ""
+ACCESS_TOKEN_SECRET = ""
+BEARER_TOKEN = "" \
+               ""
 
-# Authenticate to Twitter
-# auth = tweepy.OAuthHandler(API_KEY, API_SECRET)
-# auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-#
-# api = tweepy.API(auth)
-client = tweepy.Client(BEARER_TOKEN, API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-new_tweet = client.create_tweet(text='First tweet from #python !')
+
+def main():
+    bot_client = tweepy.Client(BEARER_TOKEN, API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)  # Initiate Client
+    previous_tweet_id = "0"
+    user = "342366464"
+
+    while True:
+        previous_tweet_id = get_latest_tweet(previous_tweet_id, bot_client, user)
+        time.sleep(600)
+
+
+def get_latest_tweet(previous_tweet_id, bot, user_id):
+    latest_tweet = bot.get_users_tweets(id=user_id)[0][0]  # Query for latest tweet from user
+    latest_tweet_id = latest_tweet.id  # Get the id of the latest tweet
+    latest_tweet_text = latest_tweet.text  # Get the text of the tweet
+
+    if latest_tweet_id != previous_tweet_id:  # If the latest tweet is new
+        print("NEW TWEET")
+        print("------------")
+        print(f'Tweet ID: {latest_tweet_id}')
+        print(f'Tweet Text: \n{latest_tweet_text}')
+        respond(latest_tweet_id, bot)
+        return latest_tweet_id
+
+    else:
+        return previous_tweet_id
+
+
+def respond(latest_tweet_id, bot):
+    bot.create_tweet(text="I'm testing the bot code. If you see this, it works.", in_reply_to_tweet_id=latest_tweet_id)
+    print("----------------------")
+    print("Responded Successfully")
+
+
+if __name__ == "__main__":
+    main()
